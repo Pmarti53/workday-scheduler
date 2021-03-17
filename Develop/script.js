4033066e68fed78e0993d218e891ddd0ad40cb47
peter <ptmartinezsound@gmail.com>
2021-03-17 14:28:31 -0500

    update

---
+++ Develop/script.js
@@ -0,0 +1,9 @@
+// n =  new Date();
+// y = n.getFullYear();
+// m = n.getMonth() + 1;
+// d = n.getDate();
+// document.querySelector("lead").innerHTML = m + "/" + d + "/" + y;
+
+$(".row row-1").on("click", "div", function () {
+    console.log("<div> was clicked");
+});

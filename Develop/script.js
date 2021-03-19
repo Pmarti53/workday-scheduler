19ae7d3b1e4966a65019c5feee0c6fa52123dc8d
peter <ptmartinezsound@gmail.com>
2021-03-19 14:40:28 -0500

    added event listeners

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,60 @@
-// n =  new Date();
-// y = n.getFullYear();
-// m = n.getMonth() + 1;
-// d = n.getDate();
-// document.querySelector("lead").innerHTML = m + "/" + d + "/" + y;
 
-$(".row row-1").on("click", "div", function () {
-    console.log("<div> was clicked");
+
+
+n =  new Date();
+y = n.getFullYear();
+m = n.getMonth() + 1;
+d = n.getDate();
+document.getElementById("currentDay").innerHTML = m + "/" + d + "/" + y;
+
+$(document).ready(function () {
+    $(".row").click(function () {
+        console.log("div was selected");
+        
+    });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // $(".row").on("click", div, function() {
+// //     console.log("<div> was clicked")
+// // })
+
+// $(document).ready(function () {
+//     $("div").click(function () {
+//         $(this).hide();
+//     });
+// });
+
+// document.getElementById("currentDay").innerHTML = "Date";
+
+// const date = date.now();
+
+// document.querySelectorAll("#times")

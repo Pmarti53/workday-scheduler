3e15278fde4138af49dc4c5ae1397295e1b4fee5
peter <ptmartinezsound@gmail.com>
2021-03-20 10:44:03 -0500

    added text area

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
+var addTask = document.getElementById('row')
 
 
-
-n =  new Date();
+n = new Date();
 y = n.getFullYear();
 m = n.getMonth() + 1;
 d = n.getDate();
@@ -10,8 +10,16 @@ document.getElementById("currentDay").innerHTML = m + "/" + d + "/" + y;
 $(document).ready(function () {
     $(".row").click(function () {
         console.log("div was selected");
-        
     });
+});
+
+jQuery(document).ready(function () {
+    $(".row").append('<p><a id=".row-1" href="#"></a></p>');
+    $('#myLink').on('click',
+        function () {
+            $('textarea').val('some content');
+        }
+    );
 });
 
 
@@ -43,18 +51,5 @@ $(document).ready(function () {
 
 
 
-// // $(".row").on("click", div, function() {
-// //     console.log("<div> was clicked")
-// // })
 
-// $(document).ready(function () {
-//     $("div").click(function () {
-//         $(this).hide();
-//     });
-// });
 
-// document.getElementById("currentDay").innerHTML = "Date";
-
-// const date = date.now();
-
-// document.querySelectorAll("#times")
